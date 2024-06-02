88ab27c75388caa813f6f0cd3f4c7cf3426d1f84
Xinyiqi <shinichi_watanabe@dignicate.com>
2024-06-02 13:57:08 +0900

    wip, mapToResource

--- composeApp/src/commonMain/kotlin/com/dignicate/zero_2024_kmp/domain/Resource.kt
+++ composeApp/src/commonMain/kotlin/com/dignicate/zero_2024_kmp/domain/Resource.kt
@@ -1,23 +1,37 @@
 package com.dignicate.zero_2024_kmp.domain
 
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
-sealed class Resource {
-    data object Initialized : Resource()
-    data object Loading : Resource()
-    data class Success<T>(val data: T) : Resource()
-    data class Error(val message: String, val throwable: Throwable) : Resource()
+sealed class Resource<out T> {
+    data object Initialized : Resource<Nothing>()
+    class Loading<T> : Resource<T>()
+    data class Success<T>(val data: T) : Resource<T>()
+    data class Failure<T>(val error: Error) : Resource<T>()
 }
 
-fun Flow<T>.mapToResource(
+@OptIn(ExperimentalCoroutinesApi::class)
+fun <S, T> Flow<S>.mapToResource(
     scope: CoroutineScope,
-    mapError: () ->
-): StateFlow<T> {
-    return map { Resource.Success(it) }
-        .onStart { emit(Resource.Loading) }
-        .catch { emit(Resource.Error(it.message ?: "Unknown error", it)) }
-        .stateIn(scope)
+    then: (S) -> Flow<T>,
+    mapError: (Throwable) -> Error = { Error.Unknown(it) },
+): StateFlow<Resource<T>> {
+    return this
+        .flatMapLatest { then(it) }
+        .map { Resource.Success(it) }
+        .onStart { emit(Resource.Loading<T>()) }
+        .catch { emit(Resource.Failure(mapError(it))) }
+        .stateIn(scope, SharingStarted.Eagerly, Resource.Initialized)
+}
+
+sealed class Error {
+    data class Unknown(val throwable: Throwable) : Error()
 }
