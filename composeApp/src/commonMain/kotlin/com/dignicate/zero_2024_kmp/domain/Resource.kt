b8e7170cb33afa49c3182b542d13e391c1d80558
Xinyiqi <shinichi_watanabe@dignicate.com>
2024-06-02 20:22:39 +0900

    Resource

--- composeApp/src/commonMain/kotlin/com/dignicate/zero_2024_kmp/domain/Resource.kt
+++ composeApp/src/commonMain/kotlin/com/dignicate/zero_2024_kmp/domain/Resource.kt
@@ -3,33 +3,57 @@ package com.dignicate.zero_2024_kmp.domain
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.flatMapLatest
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.flatMapMerge
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
 
-sealed class Resource<out T> {
+sealed class Resource<out T: Any> {
     data object Initialized : Resource<Nothing>()
-    class Loading<T> : Resource<T>()
-    data class Success<T>(val data: T) : Resource<T>()
-    data class Failure<T>(val error: Error) : Resource<T>()
+    data object InProgress : Resource<Nothing>()
+    data class Success<out T: Any>(val data: T) : Resource<T>()
+    data class Failure<out T: Any>(val error: Error) : Resource<T>()
+}
+
+fun <S: Any, T: Any> MutableSharedFlow<S>.mapToResource(
+    scope: CoroutineScope,
+    then: (S) -> Flow<Result<T>>,
+    mapError: (Throwable) -> Error = { Error.Unknown(it) },
+): StateFlow<Resource<T>> {
+    return mapToResourceAsFlow(
+        scope,
+        then,
+        mapError,
+    ).stateIn(
+        scope = scope,
+        started = SharingStarted.WhileSubscribed(),
+        initialValue = Resource.Initialized,
+    )
 }
 
 @OptIn(ExperimentalCoroutinesApi::class)
-fun <S, T> Flow<S>.mapToResource(
+fun <S: Any, T: Any> MutableSharedFlow<S>.mapToResourceAsFlow(
     scope: CoroutineScope,
-    then: (S) -> Flow<T>,
+    then: (S) -> Flow<Result<T>>,
     mapError: (Throwable) -> Error = { Error.Unknown(it) },
-): StateFlow<Resource<T>> {
-    return this
-        .flatMapLatest { then(it) }
-        .map { Resource.Success(it) }
-        .onStart { emit(Resource.Loading<T>()) }
-        .catch { emit(Resource.Failure(mapError(it))) }
-        .stateIn(scope, SharingStarted.Eagerly, Resource.Initialized)
+): Flow<Resource<T>> {
+    return flatMapMerge { param: S ->
+        flow {
+            emit(Resource.InProgress)
+            then(param).collect { result: Result<T> ->
+                result.fold(
+                    onSuccess = { data: T ->
+                        emit(Resource.Success(data))
+                    },
+                    onFailure = { throwable: Throwable ->
+                        emit(Resource.Failure(mapError(throwable)))
+                    }
+                )
+            }
+        }
+    }
 }
 
 sealed class Error {
