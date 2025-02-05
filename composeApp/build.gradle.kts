cd97b7a8fc7b7bd0e31c7c91b212a773590ba173
xinyiqiw <shinichi_watanabe@dignicate.com>
2025-02-06 00:18:13 +0900

    wip, fixing

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
-    kotlin("plugin.serialization") version "1.9.23"
+    kotlin("plugin.serialization") version "2.0.10"
 }
 
 kotlin {
