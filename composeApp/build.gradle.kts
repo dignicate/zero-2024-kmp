183f3f6506f94c954430286a17fe329942e352bf
xinyiqiw <shinichi_watanabe@dignicate.com>
2025-02-06 16:38:26 +0900

    wip, kotlin 2.0.20 etc.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -6,7 +6,8 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsCompose)
-    kotlin("plugin.serialization") version "2.0.10"
+    kotlin("plugin.serialization") version "2.0.20"
+    alias(libs.plugins.compose.compiler)
 }
 
 kotlin {
