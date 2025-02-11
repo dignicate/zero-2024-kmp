be3089129ad7e93e17efd9ed6ef0dcd4bb598907
xinyiqiw <shinichi_watanabe@dignicate.com>
2025-02-11 14:24:24 +0900

    MyCustomTheme as sample.

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,28 +1,52 @@
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import org.jetbrains.compose.resources.ExperimentalResourceApi
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 import org.lighthousegames.logging.logging
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.dignicate.zero_2024_kmp.ui.automobile.AutomobileCompanyListScreen
-
 import zero2024kmp.composeapp.generated.resources.Res
 import zero2024kmp.composeapp.generated.resources.compose_multiplatform
 
-@OptIn(ExperimentalResourceApi::class)
+private val LightColorPalette = lightColors(
+    primary = Color(0xFF6200EE),
+    secondary = Color(0xFF03DAC5),
+    background = Color.White,
+)
+
+private val DarkColorPalette = darkColors(
+    primary = Color(0xFFBB86FC),
+    secondary = Color(0xFF03DAC5),
+    background = Color.Black,
+)
+
+@Composable
+fun MyCustomTheme(content: @Composable () -> Unit) {
+    val colors = if (isSystemInDarkTheme()) DarkColorPalette else LightColorPalette
+
+    MaterialTheme(
+        colors = colors,
+        content = content
+    )
+}
+
+
 @Composable
 fun App() {
-    MaterialTheme {
+    MyCustomTheme {
         initLogger()
         initKoin()
 //        Demo()
@@ -32,7 +56,6 @@ fun App() {
     }
 }
 
-@OptIn(ExperimentalResourceApi::class)
 @Composable
 private fun Demo() {
     logging(tag = "test").d { "Demo()" }
