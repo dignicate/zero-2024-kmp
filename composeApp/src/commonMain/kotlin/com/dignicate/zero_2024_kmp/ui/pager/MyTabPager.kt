1c47600fa1498b64a6fd237a82fb6e08de6f4d16
xinyiqiw <shinichi_watanabe@dignicate.com>
2025-03-31 12:09:56 +0900

    cleanup

--- composeApp/src/commonMain/kotlin/com/dignicate/zero_2024_kmp/ui/pager/MyTabPager.kt
+++ composeApp/src/commonMain/kotlin/com/dignicate/zero_2024_kmp/ui/pager/MyTabPager.kt
@@ -1,14 +1,6 @@
 package com.dignicate.zero_2024_kmp.ui.pager
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 
 @Composable
 expect fun MyTabPager(
@@ -17,34 +9,12 @@ expect fun MyTabPager(
     onPageSelected: (Int) -> Unit,
     content: @Composable (Int) -> Unit
 )
-//{
-//    Column {
-//        Row(Modifier.fillMaxWidth()) {
-//            tabs.forEachIndexed { index, title ->
-//                Button(
-//                    onClick = { onPageSelected(index) },
-//                    modifier = Modifier.weight(1f)
-//                ) {
-//                    Text(title)
-//                }
-//            }
-//        }
-//        Box(modifier = Modifier.fillMaxSize()) {
-//            content(currentPage)
-//        }
-//    }
-//}
 
 enum class ViewState {
-    Tabs
-    ;
-//    Settings,
-//    Login
+    Tabs;
 
     fun mapToSwiftViewState(viewState: ViewState): String = when (viewState) {
         Tabs -> "tabs"
-//        ViewState.Settings -> "settings"
-//        ViewState.Login -> "login"
     }
 }
 
